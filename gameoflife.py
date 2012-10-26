2b9513558513de9ebf7333f596d1c88d802cc1bb
Robert Warner <radicalphoenix@gmail.com>
2012-10-26 16:58:22 -0700

    initial

--- gameoflife.py
+++ gameoflife.py
@@ -12,76 +12,109 @@ width = 100
 height = 100
 
 
+def all_cells():
+    for y in range(height):
+        for x in range(width):
+            yield (y, x)
+
+
+def is_alive(state, pos):
+    return state.get(pos, False)
+
+
+def get_relative_neighbours():
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if not (dx == 0 and dy == 0):
+                yield (dy, dx)
+
+
+def get_neighbours(pos):
+    '''Returns a generator that yields neighbour positions'''
+    for nrel in get_relative_neighbours():
+        yield (pos[0] + nrel[0], pos[1] + nrel[1])
+
+
+def num_neighbours(state, pos):
+    return sum(int(is_alive(state, npos)) for npos in get_neighbours(pos))
+
+
+def outcome(alive, neighbours):
+    if alive:
+        if neighbours < 2 or neighbours > 3:
+            new_cell_state = False
+        else:
+            new_cell_state = True
+    else:
+        if neighbours == 3:
+            new_cell_state = True
+        else:
+            new_cell_state = False
+
+    return new_cell_state
+
+
 def evolve(state):
     new_state = {}
 
-    for y in range(height):
-        for x in range(width):
-            alive = state.get((y, x), False)
-            neighbours = 0
-            for dx in range(-1, 2):
-                for dy in range(-1, 2):
-                    nx = x + dx
-                    ny = y + dy
-                    if nx == x and ny == y:
-                        continue
+    cells_to_check = []
+    for pos in [pos for pos in state if state[pos]]:
+        if pos not in cells_to_check:
+            cells_to_check.append(pos)
 
-                    neighbour_state = state.get((ny, nx), False)
-                    if neighbour_state:
-                        neighbours += 1
+        for npos in get_neighbours(pos):
+            if npos not in cells_to_check:
+                cells_to_check.append(npos)
 
-            if alive:
-                if neighbours < 2 or neighbours > 3:
-                    new_cell_state = False
-                else:
-                    new_cell_state = True
-            else:
-                if neighbours == 3:
-                    new_cell_state = True
-                else:
-                    new_cell_state = False
-
-            new_state[(y, x)] = new_cell_state
-
-    if DEBUG:
-        print '-----------------------'
+    for pos in cells_to_check:
+        new_state[pos] = outcome(is_alive(state, pos), num_neighbours(state, pos))
 
     return new_state
 
 
-def update_display(pad, state):
+def update_display(stdscr, state):
+    for pos in all_cells():
+        try:
+            cell_state = is_alive(state, pos)
+            if cell_state:
+                char = 'X'
+            else:
+                char = '.'
+
+            stdscr.addch(pos[0], pos[1], char)
+        except curses.error:
+            pass
+
+    stdscr.refresh()
+
+
+def update_debug_display(state):
     for y in range(height):
+        line = ''
         for x in range(width):
-            try:
-                cell_state = state.get((y, x), None)
-                if cell_state:
-                    char = 'X'
-                else:
-                    char = '-'
+            #line += str(num_neighbours(state, (y, x)))
+            line += 'X' if is_alive(state, (y, x)) else '-'
+        print line
 
-                pad.addch(y, x, char)
-            except curses.error:
-                pass
-
-    pad.refresh(0, 0, 5, 5, 20, 75)
+    print '====================='
 
 
 def main(stdscr=None):
-    if stdscr:
-        pad = curses.newpad(width, height)
-
     state = {}
 
-    coords = [(1, 0), (2, 1), (2, 2), (1, 2), (0, 2)]
+    coords = [(1, 0), (2, 1), (2, 2), (1, 2), (0, 2)]  # glider
+    #coords = [(0, 1), (1, 1), (2, 1)]  # traffic light
     for pos in coords:
         state[pos] = True
 
     while(True):
         if stdscr:
-            update_display(pad, state)
+            update_display(stdscr, state)
+        else:
+            update_debug_display(state)
 
         state = evolve(state)
-        time.sleep(.01)
+        time.sleep(0.1)
 
 if not DEBUG:
     curses.wrapper(main)
